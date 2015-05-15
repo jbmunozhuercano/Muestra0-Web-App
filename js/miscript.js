1a3f381f7872c4e69d0d713a3f1acf1333fe9117
Jorge Blas Muñoz Huércano <jorge.munoz.huercano@gmail.com>
2015-05-15 16:08:35 +0200

    Creado objeto Entrada y añadidos sus métodos.

--- js/miscript.js
+++ js/miscript.js
@@ -3,29 +3,28 @@ var url = 'http://muestra0.es/';
 var paramsId = '?json=1&include=id,title';
 var paramPost = '?json=get_post&post_id=';
 var vectorIds = [];
-var vectorTitles = [];
 
-function mostrarFecha(datos) {    
+function mostrarFecha(datos) {
     var cadena, dia, mes, anyo, string;
         
-    cadena = datos.slice(0,10); // Eliminamos la hora, minutos y segundos
-    anyo = datos.substring(0,4);
-    mes = datos.substring(5,7);
-    string = datos.substring(8,10); // Extraemos el día   
+    cadena = datos.slice(0, 10); // Eliminamos la hora, minutos y segundos
+    anyo = datos.substring(0, 4);
+    mes = datos.substring(5, 7);
+    string = datos.substring(8, 10); // Extraemos el día   
     dia = string.trim();
-    if(dia.length < 2) { dia = "0" + dia; } // Si sólo tiene un carácter, le añadimos un cero delante
+    if (dia.length < 2) { dia = "0" + dia; } // Si sólo tiene un carácter, le añadimos un cero delante
     return "Publicado el " + dia + "-" + mes + "-" + anyo; // Montamos la fecha correctamente
    
 }
 
 function montarImagen(datos) {
     var imagen = '';
-    if(jQuery.type(datos.post.attachments[0].images) !== 'array') { // Comprobamos que es un objeto
-        if(datos.post.attachments[0].images['post-thumbnail'].width == 604) {
+    
+    if (jQuery.type(datos.post.attachments[0].images) !== 'array') { // Comprobamos que es un objeto
+        if (datos.post.attachments[0].images['post-thumbnail'].width === 604) {
             var url = datos.post.attachments[0].images['post-thumbnail'].url;
-                        
-            imagen += "<img src='" + url + "' />";            
-        }        
+            imagen += "<img src='" + url + "' />";
+        }
     } else { // Si es un array cargamos una imagen por defecto
         imagen += "<img src='img/muestra0.jpg' />";
     }
@@ -35,7 +34,7 @@ function montarImagen(datos) {
 function montarComentarios(datos) {
     var cadena = "<li class='ui-li-static ui-body-inherit ui-last-child'>";
     
-    if(datos == 1) {
+    if (datos === 1) {
         cadena += datos + " comentario</li>";
     } else {
         cadena += datos + " comentarios</li>";
@@ -43,55 +42,60 @@ function montarComentarios(datos) {
     return cadena;
 }
 
+var Entrada = function () {
+    var imagen, titulo, extracto, categoria, autor, fechaPub, comentarios;
+    this.montarImagen = montarImagen;
+    this.mostrarFecha = mostrarFecha;
+    this.montarComentarios = montarComentarios;
+};
+
 function mostrarContenido(datos) {
-    var contenido = '', categoria = '', autor = '', fecha = '', comentarios = '';
-    
+    var nuevaEntrada = new Entrada();
+        
     if (datos.post.attachments.length > 0) {
-        contenido += montarImagen(datos);
+        nuevaEntrada.imagen = nuevaEntrada.montarImagen(datos);
     } else {
-        contenido += "<img src='img/muestra0.jpg' />"; // Si la entrada no tiene imagen carga una por defecto    
+        nuevaEntrada.imagen = "<img src='img/muestra0.jpg' />"; // Si la entrada no tiene imagen carga una por defecto    
     }
-    contenido += "<h1 class='ui-bar ui-bar-c ui-corner-all'>" + datos.post.title + "</h1>";
-    contenido += "<div class='ui-body ui-body-c ui-corner-all'>" + datos.post.excerpt + "</div>";
-    categoria += "Categoría: " + datos.post.categories[0].title;
-    autor += "Escrito por " + datos.post.author.name;
-    fecha += mostrarFecha(datos.post.date);
+    nuevaEntrada.titulo = "<h1 class='ui-bar ui-bar-c ui-corner-all'>" + datos.post.title + "</h1>";
+    nuevaEntrada.extracto = "<div class='ui-body ui-body-c ui-corner-all'>" + datos.post.excerpt + "</div>";
+    nuevaEntrada.categoria = "Categoría: " + datos.post.categories[0].title;
+    nuevaEntrada.autor = "Escrito por " + datos.post.author.name;
+    nuevaEntrada.fechaPub = nuevaEntrada.mostrarFecha(datos.post.date);
     
-    $('#contenidoDinamico').html(contenido);
-    $('#detalles li:nth-child(1)').html(categoria);
-    $('#detalles li:nth-child(2)').html(autor);
-    $('#detalles li:nth-child(3)').html(fecha);
+    $('#contenidoDinamico').html(nuevaEntrada.imagen + nuevaEntrada.titulo + nuevaEntrada.extracto);
+    $('#detalles li:nth-child(1)').html(nuevaEntrada.categoria);
+    $('#detalles li:nth-child(2)').html(nuevaEntrada.autor);
+    $('#detalles li:nth-child(3)').html(nuevaEntrada.fechaPub);
     $('#detalles li:nth-child(4)').remove();
     
-    if(datos.post.comment_count > 0) { // Si tiene comentarios, muestra cuantos
-        comentarios += montarComentarios(datos.post.comment_count);
+    if (datos.post.comment_count > 0) { // Si tiene comentarios, muestra cuantos tiene
+        nuevaEntrada.comentarios = nuevaEntrada.montarComentarios(datos.post.comment_count);
         $('#detalles li:nth-child(3)').removeClass('ui-last-child'); // Le quitamos la clase de borde inferior redondeado para que lo tenga el siguiente elemento
-        $('#detalles').append(comentarios); // Añadimos el nodo de comentario a la lista desordenada
+        $('#detalles').append(nuevaEntrada.comentarios); // Añadimos el nodo de comentario a la lista desordenada
     }
     
-   
-    $('#').html(comentarios);
 }
 
 function mostrarMensajeError() {
-    setTimeout(function() { 
-        $.mobile.loading('show',{ // Muestra el widget de carga
+    setTimeout(function () {
+        $.mobile.loading('show', { // Muestra el widget de error
             text: '¡Falló la conexión! Comprueba tu conexión a internet y vuelve a intentarlo.',
             textVisible: true,
             textonly: true,
             theme: 'b'
-        }); 
-    },5);    
+        });
+    }, 5);
 }
 
 function mostrarMensajeCarga() {
-    setTimeout(function() { 
-        $.mobile.loading('show',{ // Muestra el widget de carga
+    setTimeout(function() {
+        $.mobile.loading('show', { // Muestra el widget de carga
             text: 'Cargando...',
             textVisible: true,
             theme: 'b'
-        }); 
-    },1);    
+        });
+    }, 1);
 }
 
 function obtenerIds() {
@@ -101,16 +105,16 @@ function obtenerIds() {
         type: 'GET',
         contentType: 'text/javascript',
         beforeSend: mostrarMensajeCarga(),
-        success: function(data) {            
+        success: function (data) {
             $('#subMenu li a').each(function (i) {
                 vectorIds.push(data.posts[i].id); // Almacenamos el id de los últimos posts
                 $(this).text(data.posts[i].title); // Mostramos el título de cada post en el ancla
-            });            
+            });
         },
-        complete: function() {
-            setTimeout(function(){
+        complete: function () {
+            setTimeout(function () {
                 $.mobile.loading('hide'); // Ocultamos el widget de carga
-            },1);    
+            }, 1);
         }
     });
 }
@@ -123,16 +127,16 @@ function obtenerPost(posicionVector) {
         type: 'GET',
         contentType: 'text/javascript',
         beforeSend: mostrarMensajeCarga(),
-        success: function(data) {
+        success: function (data) {
             mostrarContenido(data);
-            $('#contenidoDinamico a').attr('data-ajax','false'); // Añadimos el data-ajax para las páginas fuera de la app
+            $('#contenidoDinamico a').attr('data-ajax', 'false'); // Añadimos el data-ajax para las páginas fuera de la app
         },
-        complete: function() {
-            setTimeout(function(){
+        complete: function () {
+            setTimeout(function () {
                 $.mobile.loading('hide'); //Ocultamos el widget de carga
-            },1);    
+            }, 1);
         },
-        error: function(jqXHR, textStatus, errorThrown) {
+        error: function (jqXHR, textStatus, errorThrown) {
             mostrarMensajeError();
         }
     });
@@ -141,21 +145,22 @@ function obtenerPost(posicionVector) {
 function anyadirEventos() {
     var arrayElementos = $('#subMenu li a');
         
-    arrayElementos.each(function(i){
+    arrayElementos.each(function (i) {
                     
-        $(this).on('click', function(){
-            obtenerPost(i);        
+        $(this).on('click', function () {
+            obtenerPost(i);
         });
     
     });
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
     
     obtenerIds();
-    obtenerPost(0); 
     anyadirEventos();
-    $('nav > ul > li:nth-child(2)').on('click', function() { //Muestra y oculta el submenu de navegación
+    obtenerPost(0);
+    
+    $('nav > ul > li:nth-child(2)').on('click', function () { //Muestra y oculta el submenu de navegación
         $('nav > ul ul').fadeToggle();
     });
 });
